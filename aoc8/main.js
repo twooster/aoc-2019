91c05c2950e793c1645903977174946484eaf92f
Tony Wooster <tony.wooster@contentful.com>
2019-12-08 19:09:19 +0100

    Add day 8 part two

--- aoc8/main.js
+++ aoc8/main.js
@@ -4,3 +4,12 @@ require('fs').readFile('./input.txt', 'utf8', (_, c) =>
         .map((_, i) => d.slice(i*z, i*z+z).reduce((a, v) => (a[v] = (a[v] || 0) + 1, a), {}))
         .reduce((a, v) => v[0] < a[0] ? v : a))
     )(c.split(''), 25*6)))
+
+require('fs').readFile('./input.txt', 'utf8', (_, c) =>
+    console.log(((d, w, h, z=w*h) =>
+      (d => Array(h).fill(0).map((_, i) => d.slice(i*w, i*w+w).map(z => [' ', '*', ' '][z]).join('')).join('\n'))(
+        Array((d.length / z)|0).fill(0)
+          .map((_, i) => d.slice(i*z, i*z+z))
+          .reduce((a, v) => a.map((w, i) => w === 2 ? v[i] : w))
+      )
+    )(c.split('').map(x=>+x), 25, 6)))
